bc4cfd0190c0a1cd01deaf9e0ac33e94f6df9d79
Ming Rong <kwekmingrong@yahoo.com.sg>
2023-04-10 16:13:34 +0800

    latest R shiny 4.13pm

--- SpatialBros/app.R
+++ SpatialBros/app.R
@@ -2,8 +2,6 @@
 # You can run the application by clicking 
 # the 'Run App' button above.
 
-
-
 library(shiny)
 library(shinythemes)
 
@@ -16,9 +14,7 @@ ui <- fluidPage(theme = shinytheme("united"),
                    column(4,
                           h2("Project Motivation"),
                           hr(),
-                          h4("In today’s technological advancing world, there are many useful and interesting spatial data sources that exist in the forms of Geospatial and Aspatial format. Geographical Geospatial data sets the foundation based on the geographical boundary locations and the Aspatial data are the records of observation that can be further prepared to be used to derive meaningful insights."),
-                          h4("Despite all the data sources out on the interweb, there are not many people who are knowledgeable and trained to perform such analysis. Without the fundamental knowledge and training involved, any results based on the analysis performed could result in inaccuracies.
-                          Our group attempt is to mainly focus on performing analysis and develop a website based geographical spatial tool. R Shiny tool will be used with regards to developing the 1st/2nd Order & Network Constrained Point Pattern Analysis of Melbourne City, Australia.")),
+                          uiOutput("projectMotivation")),
                    column(4,
                           h2("Road network with childcare centres in Punggol, Singapore"),
                           hr(),
@@ -32,30 +28,71 @@ ui <- fluidPage(theme = shinytheme("united"),
                    column(8,
                           h2("What is Point Pattern Analysis and Network Constrained Point Pattern Analysis?"),
                           hr(),
-                          h4("Point Pattern Analysis methods helps provide insights about where things occur, how the distribution of incidents or the arrangement of data aligns with other features in the landscape, and what the patterns may reveal about potential connections and correlations."),
-                          h4("Network constrained Spatial Point Patterns Analysis (NetSPAA) is a collection of spatial point patterns analysis methods special developed for analysing spatial point event occurs on or alongside network. The spatial point event can be locations of childcare centre for example. The network, on the other hand can be a road network or river network.")),
+                          uiOutput("ppancppa")),
                    ),
                  fluidRow(
                    column(8,
                           h2("About Spatial Bros"),
                           hr(),
-                          h4("Spatial Bros is created to assist non technologically savvy users in performing geographical point pattern 
-                             analysis. This application aims to assist users in 2 types of analysis, particularly in performing 1st/2nd Order & Network Constrained Spatial Point 
-                             Pattern Analysis. For each of the analyses, the application will provide users with statistical functions, kernel density heat map estimation, various mappings and G&K function results. 
-                             The application will cover an array of spatial points located in Melbourne City such as childcare centres, business establishments, famous landmarks including places of interest such as schools, theaters, health service, 
-                             sports facilities, drinking fountains and public toilets. The spatial points will work in conjunction to cover areas of the city’s road, pedestrian and tram network. From this application, users would be able to perform types 
-                             of hypothesis testing that allow them to generate insights towards their conclusion on the distribution along the spatial points along the network."),
-                          ),
+                          uiOutput("aboutus")),
                    )
                  ),
-        tabPanel("Spatial Point Pattern Analysis"),
-        tabPanel("Network Constrained Point Pattern Analysis"))
-      )          
+        # 2nd Tab
+        
+        tabPanel("Spatial Point Pattern Analysis",
+                 titlePanel("Kernel Density Estimation"),
+                 ),
+        
+        # 3rd Tab
+        tabPanel("Network Constrained Point Pattern Analysis",
+                 titlePanel("Network Kernel Density Estimation"),
+                 sidebarLayout(
+                   sidebarPanel(
+                     "Network Kernel Density Estimation Variable Inputs",
+                     selectInput(inputId = "network_type",
+                                 label = "Types of Network",
+                                 choices = list("Road Network" = "road_network",
+                                                "Pedestrian Network" = "pedestrian_network",
+                                                "Tram Network" = "tram_network"),
+                                 selected = "Childcare Centres"),
+                     selectInput(inputId = "location_of_interest",
+                                 label = "Location of Interest",
+                                 choices = list("Childcare Centres" = "childcare_sp_sf",
+                                                "Business Establishments" = "business_est_sp",
+                                                "Drinking Fountain" = "drinking_fountain_sp",
+                                                "Landmarks" = "landmarks_sp",
+                                                "Public Toilets" = "public_toilets_sp_sf"),
+                                 selected = "Childcare Centres"),
+                     selectInput(inputId = "kernel_name",
+                                 label = "Choose the kernel to be used:",
+                                 choices = list("Quartic" = "quartic",
+                                                "Triangle" = "triangle",
+                                                "Tricube" = "tricube",
+                                                "Consine" = "cosine",
+                                                "Triweight" = "triweight",
+                                                "Epanechnikov" = "epanechnikov",
+                                                "Uniform" = "uniform"),
+                                 selected = "quartic"),
+                     selectInput(inputId = "method_name",
+                                 label = "Select the Method to be used",
+                                 choices = list("Simple" = "simple",
+                                                "Discontinuous" = "discontinuous",
+                                                "Continuous" = "Continuous"),
+                                 selected = "simple"),
+                 ),
+                 mainPanel(
+                   "Network KDE goes here..."
+                 ),
+            )
+        )
+      )
+)
 
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     
+    # Images
     output$childcare_image <- renderImage({
       list(src = "www/examplechildcare.png",
            wdith = "100%",
@@ -63,6 +100,22 @@ server <- function(input, output) {
            align = 'center')
     },
     deleteFile = F)
+    
+    # Home page UI
+    output$projectMotivation <- renderUI(HTML("<h4> In today’s technological advancing world, there are many useful and interesting spatial data sources that exist in the forms of Geospatial and Aspatial format. Geographical Geospatial data sets the foundation based on the geographical boundary locations and the Aspatial data are the records of observation that can be further prepared to be used to derive meaningful insights. 
+                                              </h4> 
+                                              <h4>Despite all the data sources out on the interweb, there are not many people who are knowledgeable and trained to perform such analysis. Without the fundamental knowledge and training involved, any results based on the analysis performed could result in inaccuracies.
+                          Our group attempt is to mainly focus on performing analysis and develop a website based geographical spatial tool. R Shiny tool will be used with regards to developing the 1st/2nd Order & Network Constrained Point Pattern Analysis of Melbourne City, Australia.<h4>"))
+    
+    output$ppancppa <- renderUI(HTML("<h4> Point Pattern Analysis methods helps provide insights about where things occur, how the distribution of incidents or the arrangement of data aligns with other features in the landscape, and what the patterns may reveal about potential connections and correlations. <h4>
+                                     <h4> Network constrained Spatial Point Patterns Analysis (NetSPAA) is a collection of spatial point patterns analysis methods special developed for analysing spatial point event occurs on or alongside network. The spatial point event can be locations of childcare centre for example. The network, on the other hand can be a road network or river network. <h4>"))
+    
+    output$aboutus <- renderUI(HTML("<h4> Spatial Bros is created to assist non technologically savvy users in performing geographical point pattern 
+                             analysis. This application aims to assist users in 2 types of analysis, particularly in performing 1st/2nd Order & Network Constrained Spatial Point 
+                             Pattern Analysis. For each of the analyses, the application will provide users with statistical functions, kernel density heat map estimation, various mappings and G&K function results. 
+                             The application will cover an array of spatial points located in Melbourne City such as childcare centres, business establishments, famous landmarks including places of interest such as schools, theaters, health service, 
+                             sports facilities, drinking fountains and public toilets. The spatial points will work in conjunction to cover areas of the city’s road, pedestrian and tram network. From this application, users would be able to perform types 
+                             of hypothesis testing that allow them to generate insights towards their conclusion on the distribution along the spatial points along the network. </h4>"))
     
     output$myList <- renderUI(HTML("<h4>A Group Project done by<h4>
                                    <ul>
@@ -78,18 +131,9 @@ server <- function(input, output) {
                                    <p> This project is done for IS415 Geospatial Analytics & Application under the guidance of Professor Kam Tin Seong </p>
                                    <br>
                                    <img src = 'smu.png' width = 50%, height = 50%>"))
-    
-                                    
-    # output$distPlot <- renderPlot({
-    #     # generate bins based on input$bins from ui.R
-    #     x    <- faithful[, 2]
-    #     bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    # 
-    #     # draw the histogram with the specified number of bins
-    #     hist(x, breaks = bins, col = 'darkgray', border = 'white',
-    #          xlab = 'Waiting time to next eruption (in mins)',
-    #          main = 'Histogram of waiting times')
-    # })
+
+                              
+
 }
 
 # Run the application 
